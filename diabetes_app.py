81b69dd2292150b0686f7aeb7fe62c8be22ef7ba
Soheil Salemi <94727949+Soheils78@users.noreply.github.com>
2024-11-12 17:39:24 +0000

    Update diabetes_app.py

--- diabetes_app.py
+++ diabetes_app.py
@@ -13,9 +13,9 @@ data[['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']] = data[['G
 # Fill NaN values with the mean of each column
 data.fillna(data.mean(), inplace=True)
 
-# Standardize the features
+# Standardize the features, including Age
 scaler = StandardScaler()
-X = scaler.fit_transform(data[['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']])
+X = scaler.fit_transform(data[['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'Age']])
 y = data['Outcome']
 
 # Train the Logistic Regression model with an 80-20 split
@@ -23,23 +23,22 @@ model = LogisticRegression(random_state=42)
 model.fit(X, y)
 
 # Streamlit app
-st.title("Diabetes Prediction Apps")
+st.title("Diabetes Prediction App")
 st.write("This app predicts the likelihood of diabetes based on user inputs.")
 st.write("Implemented by Soheil Salemi")
 
-
 # User input fields
 glucose = st.number_input("Glucose Level", min_value=0.0, max_value=300.0, value=120.0)
-blood_pressure = st.number_input("Blood Pressure (Diastolic blood pressure (mm Hg))", min_value=0.0, max_value=200.0, value=70.0)
-skin_thickness = st.number_input("Skin Thickness (Triceps skin fold thickness (mm))", min_value=0.0, max_value=100.0, value=20.0)
-insulin = st.number_input("Insulin Level (2-Hour serum insulin (mu U/ml))", min_value=0.0, max_value=900.0, value=80.0)
-bmi = st.number_input("BMI (Body mass index (weight in kg/(height in m)^2))", min_value=0.0, max_value=100.0, value=25.0)
-# age = st.number_input("Age", min_value=0, max_value=100, value=25)
+blood_pressure = st.number_input("Blood Pressure (Diastolic blood pressure in mm Hg)", min_value=0.0, max_value=200.0, value=70.0)
+skin_thickness = st.number_input("Skin Thickness (Triceps skin fold thickness in mm)", min_value=0.0, max_value=100.0, value=20.0)
+insulin = st.number_input("Insulin Level (2-Hour serum insulin in mu U/ml)", min_value=0.0, max_value=900.0, value=80.0)
+bmi = st.number_input("BMI (Body mass index in kg/m²)", min_value=0.0, max_value=100.0, value=25.0)
+age = st.number_input("Age", min_value=0, max_value=120, value=30)
 
 # Predict button
 if st.button("Predict"):
     # Preprocess the user input
-    user_data = np.array([[glucose, blood_pressure, skin_thickness, insulin, bmi]])
+    user_data = np.array([[glucose, blood_pressure, skin_thickness, insulin, bmi, age]])
     user_data_scaled = scaler.transform(user_data)
 
     # Make the prediction
